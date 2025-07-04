9775c717cb2ea64331a860f00d9efc0a7d52ae93
quanlee01 <quanmcvn91@gmail.com>
2025-07-04 12:20:21 +0700

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int isEven(int a)
+{
+    return (a%2==0);
+}
+int main()
+{
+    char b[3][3] = {{'.'}}; 
+    bool end = false;
+    int cnt = 0;
+    char m[2] = {'O','X'};
+    while(!end)
+    {
+        if(9 == cnt) end = ~end;
+        for(int i = 0; i < 3; ++i)
+        {
+            for(int j = 0; j < 3; ++j)
+            {
+                cout<<b[i][j]<<' ';
+            }
+            cout<<'\n';
+        }
+        int x,y;
+        cout<<"Your next move (type in the coordinate): "; cin>>x>>y;
+        if(b[x][y] == 'X' or b[x][y] == 'O' or x > 2 or y > 2)
+        {
+            cout<<"Invalid move"<<'\n'; 
+        }
+        else
+        {
+            
+        }
+    return 0;
+}
